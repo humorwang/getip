75938d240772b0075884a07e5eb78d394ca8c139
wy <wangyan4170@gmail.com>
2023-07-16 14:39:58 +0800

    up

--- main.go
+++ main.go
@@ -102,10 +102,28 @@ func response(c *gin.Context) {
 func init() {
 	flag.StringVar(&port, "port", ":8080", "端口")
 }
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+    c.Header("Access-Control-Allow-Origin", "*")  // 可将将 * 替换为指定的域名
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			c.AbortWithStatus(204)
+		}
+		c.Next()
+	}
+}
+
+
 func main() {
 	flag.Parse()
 	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
+	r.Use(Cors())
 	v1 := r.Group("/")
 	v1.GET("/*router", response)
 	v1.HEAD("/*router", response)
